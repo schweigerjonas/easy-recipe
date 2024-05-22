d6af65628f37b17f25b8bd90a28012e6c7c03308
Jonas Schweiger <jonas.schweiger91@gmail.com>
2024-05-22 17:23:59 +0200

    Add basic gridView for recipes

--- lib/home.dart
+++ lib/home.dart
@@ -12,6 +12,40 @@ class _HomePageState extends State<HomePage> {
   NavigationDestinationLabelBehavior labelBehavior =
       NavigationDestinationLabelBehavior.onlyShowSelected;
 
+  List<Card> _buildRecipeCards(int count) {
+    List<Card> cards = List.generate(
+      count,
+      (int index) {
+        return const Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 18.0 / 11.0,
+                //IMAGES ARE SHOWN IN THIS CONTAINER
+                //child ,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('TITLE'),
+                    SizedBox(height: 8.0),
+                    Text('SCHWIERIGKEIT | DAUER'),
+                    SizedBox(height: 8.0),
+                    Text('BESCHREIBUNG'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+    return cards;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +68,7 @@ class _HomePageState extends State<HomePage> {
                 const Expanded(
                   child: TextField(
                     decoration: InputDecoration(
-                      icon: Icon(Icons.search),
+                      prefixIcon: Icon(Icons.search),
                       hintText: 'Suche',
                       border: OutlineInputBorder(),
                       fillColor: Color(0xFFFFFFFF),
@@ -43,20 +77,28 @@ class _HomePageState extends State<HomePage> {
                 ),
                 const SizedBox(width: 8.0),
                 ElevatedButton(
-                  onPressed: () {
-
-                  },
-                  child: const Icon(
-                      Icons.filter_list,
-                      color: Color(0xFF367D5F)
-                  ),
+                  onPressed: () {},
+                  child:
+                      const Icon(Icons.filter_list, color: Color(0xFF367D5F)),
                 ),
               ],
             ),
           ),
-          
+          Container(
+            padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 8.0),
+            alignment: Alignment.centerLeft,
+            child: const Text('Beliebte Rezepte'),
+          ),
+          Expanded(
+            child: GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 1,
+              padding: const EdgeInsets.all(16.0),
+              childAspectRatio: 8.0 / 9.0,
+              children: _buildRecipeCards(10),
+            ),
+          ),
         ],
-
       ),
 
       //Navigation
@@ -71,27 +113,28 @@ class _HomePageState extends State<HomePage> {
         destinations: const <Widget>[
           NavigationDestination(
             icon: Icon(
-                Icons.book,
-                color: Color(0xFF367D5F),
+              Icons.book,
+              color: Color(0xFF367D5F),
             ),
             label: 'Meine Rezepte',
           ),
           NavigationDestination(
             icon: Icon(
-                Icons.home,
-                color: Color(0xFF367D5F),
+              Icons.home,
+              color: Color(0xFF367D5F),
             ),
             label: 'Home',
           ),
           NavigationDestination(
             icon: Icon(
-                Icons.person,
-                color: Color(0xFF367D5F),
+              Icons.person,
+              color: Color(0xFF367D5F),
             ),
             label: 'Profil',
           ),
         ],
       ),
+      resizeToAvoidBottomInset: false,
     );
   }
 }
