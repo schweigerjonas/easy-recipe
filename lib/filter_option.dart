b0fd22b7bda2f52434f7be40b3d940b042c6ccf7
Jonas Schweiger <jonas.schweiger91@gmail.com>
2024-05-23 09:37:32 +0200

    Add multi select for filter options button

---
+++ lib/filter_option.dart
@@ -0,0 +1,9 @@
+class FilterOption {
+  final int id;
+  final String name;
+
+  FilterOption({
+    required this.id,
+    required this.name,
+  });
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:multi_select_flutter/multi_select_flutter.dart';
+
+import 'filter_option.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +14,54 @@ class _HomePageState extends State<HomePage> {
   int currentPageIndex = 1;
   NavigationDestinationLabelBehavior labelBehavior =
       NavigationDestinationLabelBehavior.onlyShowSelected;
+
+  static List<FilterOption> _filterOptions = [
+    FilterOption(id: 0, name: 'bis 20min'),
+    FilterOption(id: 1, name: '20 bis 40min'),
+    FilterOption(id: 2, name: '40 bis 60min'),
+    FilterOption(id: 3, name: 'länger als 60min'),
+    FilterOption(id: 4, name: '2 Portionen'),
+    FilterOption(id: 5, name: '3 Portionen'),
+    FilterOption(id: 6, name: '4 Portionen'),
+    FilterOption(id: 7, name: 'vegetarisch'),
+    FilterOption(id: 8, name: 'vegan'),
+    FilterOption(id: 9, name: 'laktosefrei'),
+    FilterOption(id: 10, name: 'glutenfrei'),
+  ];
+
+  final _items = _filterOptions
+      .map((filterOption) => MultiSelectItem<FilterOption>(filterOption, filterOption.name))
+      .toList();
+
+  List<FilterOption> _selectedFilterOptions = [];
+
+  @override
+  void initState() {
+    _selectedFilterOptions = [];
+    super.initState();
+  }
+
+  void _showMultiSelect(BuildContext context) async {
+    await showModalBottomSheet(
+      isScrollControlled: true, // required for min/max child size
+      context: context,
+      builder: (ctx) {
+        return  MultiSelectBottomSheet(
+          title: const Padding(
+            padding: EdgeInsets.fromLTRB(14.0, 8.0, 0.0, 0.0),
+            child: Text('Filter'),
+          ),
+          listType: MultiSelectListType.CHIP,
+          items: _items,
+          initialValue: _selectedFilterOptions,
+          onConfirm: (values) {
+            _selectedFilterOptions = values;
+          },
+          maxChildSize: 0.8,
+        );
+      },
+    );
+  }
 
   List<Card> _buildRecipeCards(int count) {
     List<Card> cards = List.generate(
@@ -77,7 +128,9 @@ class _HomePageState extends State<HomePage> {
                 ),
                 const SizedBox(width: 8.0),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    _showMultiSelect(context);
+                  },
                   child:
                       const Icon(Icons.filter_list, color: Color(0xFF367D5F)),
                 ),
